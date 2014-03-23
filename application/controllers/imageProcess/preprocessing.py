cb1a6b99a3b4927036031646cd4bd7de3d4a8fb9
tiffanyrp <tiffrp96@gmail.com>
2014-03-23 02:14:56 -0400

    [new] whitespace trim

--- application/controllers/imageProcess/preprocessing.py
+++ application/controllers/imageProcess/preprocessing.py
@@ -1,4 +1,13 @@
-import cv2
+from PIL import Image, ImageChops
 
-def preprocessing(img):
-	
+def trim(im):
+    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+
+im = Image.open("example-rosemary.jpg")
+im = trim(im)
+im.show()
