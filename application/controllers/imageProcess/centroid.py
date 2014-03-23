885e62f686d0097837c8747238d299c2dbb83430
yashpatel5400 <yashpatel5400@gmail.com>
2014-03-22 23:54:58 -0400

    [new] Added centroid calculation (Python file)

--- application/controllers/imageProcess/centroid.py
+++ application/controllers/imageProcess/centroid.py
@@ -1,3 +1,22 @@
-import cv2
+from PIL import Image, ImageFilter
+im = Image.open(fileName)
 
-def centroid(img):
+def findCentroid(im):
+    width, height = im.size
+    XX, YY, count = 0, 0, 0
+    for x in xrange(0, width, 1):
+        for y in xrange(0, height, 1):
+            if im.getpixel((x, y)) == (255,255,255):
+                XX += x
+                YY += y
+                count += 1
+
+    final_x = XX/count
+    final_y = YY/count
+    finalArray = []
+    finalArray.append(final_x)
+    finalArray.append(final_y)
+    return finalArray
+
+if __name__ == "__main__":
+    print findCentroid(BlackBox.jpg)
