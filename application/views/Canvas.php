3c31113289ac097d1a08610b5784e0f9e38a998a
Michelle <emceji@gmail.com>
2014-03-22 20:09:58 -0400

    Fix the script src in canvas.php

---
+++ application/views/Canvas.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Canvas</title>
+    <link rel="stylesheet" type="text/css" href="file:///Users/Macbook/Desktop/HandRighting2/css/bootstrap.css"> 
+    <style type="text/css"><!--
+      #container { position: relative; }
+      #imageView { border: 2px solid #000; }
+    --></style>
+  </head>
+  <body>
+    <h1> Please clearly print your password into the canvas below.</h1>
+    <div id="container">
+      <canvas id="imageView" width="800" height="250">
+      </canvas>
+    </div>
+
+    <script type="text/javascript" src="file:///Users/Macbook/Desktop/Canvas.js"></script>
+  </body>
+</html>
+
