10b26f9aa432e14f0953c1d1299f31b631ddfb41
yashpatel5400 <yashpatel5400@gmail.com>
2014-03-22 21:33:03 -0400

    [new] Fixed navbar

--- application/views/landingendF.php
+++ application/views/landingendF.php
@@ -32,13 +32,15 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="http://startbootstrap.com">Pass Your Word</a>
+            <a class="navbar-brand" href="http://startbootstrap.com">Hand Righting</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="#about">Learn Mor(se)!</a>
+                <li><a href="#contact">Contact Us</a></li>
+                <li>
+                    <a href="#about">About</a>
                 </li>
             </ul>
         </div>
