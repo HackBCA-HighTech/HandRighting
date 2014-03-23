fbdda2f553abbf3460ae8a0c573521bf6c096760
tiffanyrp <tiffrp96@gmail.com>
2014-03-23 02:48:37 -0400

    fix the encryption error

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'O5y8j10lu64wQHC14RIL3Nv4W4xtPd7j';
 
 /*
 |--------------------------------------------------------------------------
