9e2e3a217bf073a03a8a351b9104e83f1f5a5628
yashpatel5400 <yashpatel5400@gmail.com>
2014-03-23 08:47:22 -0400

    [new] More colors

--- application/views/Canvas.php
+++ application/views/Canvas.php
@@ -8,9 +8,9 @@
     <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
 
     <style type="text/css">
-        body { background: darkturquoise;}
+        body { background: rgb(30, 108, 109);}
         #container { position: relative;}
-      #imageView { border: 0px solid #000; border-radius: 10px; background-color: #eee; margin-left:60px}
+      #imageView { border: 0px solid #000; border-radius: 10px; background-color: #ffffff; margin-left:60px}
     --></style>
   </head>
 
